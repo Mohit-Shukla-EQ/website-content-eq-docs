d58b89cc2c51b085bd01fa081567c0305ca68a21
Mohit-Shukla-EQ <87744566+Mohit-Shukla-EQ@users.noreply.github.com>
2021-08-30 18:29:59 +0530

    Add files via upload

---
+++ collections/texts.js
@@ -0,0 +1,21 @@
+const markdownList = [
+  {
+    content: `
+  # Header 1
+  ## Header 2
+  *italic*
+  **bold**
+
+  list
+  - asas
+  - asasas
+    - asasas
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+>
+> The Witch bade her clean the pots and kettles and sweep the floor and keep the fire fed with wood.
+
+  `,
+  },
+];
+export default markdownList;
